cee5dbdfbef59bc00c74ba1c8b58581b0235bbde
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2018-04-14 21:48:32 +1200

    Copy the src dir anyway so that ZScript includes work

--- compile.sh
+++ compile.sh
@@ -241,13 +241,13 @@ echo
 # now we need to copy all the files
 # and directories that we haven't already
 for src_file in src/*; do
-    if ! [[ $src_file = src/src ]]; then
+  # if ! [[ $src_file = src/src ]]; then
         echo Copying $src_file
 
         destination=bin/build/${src_file#src/}
         mkdir -p $(dirname "${destination}") && cp -r $src_file $destination
 
-    fi
+  # fi
 done
 
 echo Compilation complete.
